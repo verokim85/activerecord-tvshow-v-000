e7239efefb4454a8abdd8a322af4464d7d40977c
Veronica Kim <vero.a.kim@gmail.com>
2018-05-08 23:39:44 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,7 @@
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
-      t.sting :name
+      t.string :name
       t.string :network
       t.string :day
       t.integer :rating
